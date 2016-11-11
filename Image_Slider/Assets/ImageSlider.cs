a02a7a48e7ebfedbd42034a6ce0e1bcfca8b762a
MrElektronix <milovanpelt@gmail.com>
2016-11-11 10:58:42 +0100

    boop

--- Image_Slider/Assets/ImageSlider.cs
+++ Image_Slider/Assets/ImageSlider.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
+
 public class ImageSlider : MonoBehaviour
 {
+   TimerForSlider timerforslider;
+
     public Sprite[] Images;
     //public Sprite _currentImage;
     public Image displayImage;
@@ -10,33 +12,53 @@ public class ImageSlider : MonoBehaviour
     public Button PreviousButton;
     public int i;
 
+
     // Use this for initialization
     void Start()
     {
-
+        timerforslider = TimerForSlider.instance;
+    
     }
-
-    // Update is called once per frame
+     
     void Update()
     {
         displayImage.sprite = Images[i];
+        Dia();
     }
 
     public void Previous()
     {
+   
         if (i > 0)
         {
             i--;
             Debug.Log(displayImage);
         }
+        else
+        {
+            i = Images.Length - 1;
+        }
     }
 
     public void Next()
     {
-        if (i < Images.Length  - 1)
+        if (i < Images.Length - 1)
         {
             i++;
             Debug.Log(displayImage);
+        }
+        else {
+            i = 0;
+        }
+    }
+
+    public void Dia()
+    {
+        timerforslider.ImageTime -= 1f * Time.deltaTime;
+        if (timerforslider.ImageTime <= 0)
+        {
+            timerforslider.ImageTime = 5f ;
+            Next();
         }
     }
 
---
+++ Image_Slider/Assets/TimerForSlider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TimerForSlider : MonoBehaviour {
+    public static TimerForSlider instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public float ImageTime;
+
+	// Update is called once per frame
+	void Update () {
+        if (ImageTime <= 0)
+        {
+            ImageTime = 0;
+        } else
+        {
+            Debug.Log(ImageTime);
+        }
+	}
+}
